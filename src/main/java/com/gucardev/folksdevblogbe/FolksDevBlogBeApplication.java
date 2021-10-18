9429bf30ad8747330964db0eac3847b02a2f2d35
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-10-18 11:18:12 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/folksdevblogbe/FolksDevBlogBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.folksdevblogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolksDevBlogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolksDevBlogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/folksdevblogbe/FolksDevBlogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.folksdevblogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolksDevBlogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
